6517d24e4c4a3efbb80fe4b284d99055a1cf6593
zedvas <zahravasanji@hotmail.com>
2024-06-17 21:07:34 +0100

    accessed data correctly

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -15,8 +15,17 @@ class Section extends Component {
     } = entry;
 
     return (
-      <div> {Object.entries(entry).map(item=> {
-        return <p>{item[0]}</p>
+      <div> 
+      {/* {Object.entries(entry).map(item=> {
+        return <p>{item[0]}{item[1]}</p>
+        })} */}
+      {Object.entries(entry).map(headings=> {
+        {/* return <h2>{headings[0]}</h2>   //headings[0] for heading */}
+        headings[1].map(experience=> {
+            Object.entries(experience).map(data => {
+                console.log(headings[0], data[0],data[1])
+            })
+        })
         })}
   
       </div>
